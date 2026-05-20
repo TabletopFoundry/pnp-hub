b2a5875594d9f78d2583fe37c5acedb739b2721b
Jose David Baena <josedab@gmail.com>
2026-05-20 08:36:22 +0200

    chore(lint): ignore generated website artifacts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTypescript from 'eslint-config-next/typescript';
 
-const config = [{ ignores: ['coverage/**', '.next/**'] }, ...nextVitals, ...nextTypescript];
+const config = [{ ignores: ['coverage/**', '.next/**', 'website/.docusaurus/**', 'website/build/**'] }, ...nextVitals, ...nextTypescript];
 
 export default config;
